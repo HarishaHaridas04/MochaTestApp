1a832f6436d57e29577c5c406dc4841539897d8d
Vishakh <vish0004@algonquinlive.com>
2023-09-21 14:58:48 -0400

    Initial commit

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class Calculator extends Component {
+  // Calculator logic goes here
+
+  render() {
+    return (
+      <div className="container">
+        {/* Calculator UI goes here */}
+      </div>
+    );
+  }
+}
+
+export default Calculator;
---
+++ src/screens/Home.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class Home extends Component {
+
+  render() {
+    return (
+      <div className="container">
+      </div>
+    );
+  }
+}
+
+export default Home;
